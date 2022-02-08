0824144fe433f5d6697c0b5a51acf64d240c107c
Roland Salloum <roland.salloum00@outlook.com>
2022-02-08 16:54:16 +0200

    feat: yolov4-deepsort endpoints

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from enum import Enum
 import os
 from fastapi import FastAPI, File, UploadFile
 from starlette.responses import FileResponse
+import tensorflow as tf
+from tensorflow.python.saved_model import tag_constants
+from os import walk
+
+from object_tracker_endpoint import Process
+
+saved_model_loaded = tf.saved_model.load('./checkpoints/yolov4-416', tags=[tag_constants.SERVING])
 
 app = FastAPI()
 
@@ -11,7 +18,6 @@ app = FastAPI()
 
 class ModelName(str, Enum):
     yolov4 = "yolov4"
-
 
 
 @app.get("/api/v1/public")
@@ -33,7 +39,6 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "Darknet YOLOV4 Available": True, "Tensorflow YOLOV4 Available": True, }
 
 
-
 @app.post("/upload-video")
 async def uploadVideo(file: UploadFile = File(...)):
     with open('./video_received/{}'.format(file.filename), 'wb') as buffer:
@@ -44,13 +49,26 @@ async def uploadVideo(file: UploadFile = File(...)):
 
 @app.get("/getVideo/{path}")
 def getVideo(path):
-    file_path = os.path.join("video_received/{}".format(path))
+    file_path = os.path.join("video_processed/{}".format(path))
     if os.path.exists(file_path):
-            return FileResponse(file_path, media_type="video/mp4", filename="{}.mp4".format(path))
-    return {"error" : "File not found!"}
+        return FileResponse(file_path, media_type="video/mp4", filename="{}.mp4".format(path))
+    return {"error": "File not found!"}
 
 
+@app.get("/processed-videos")
+def getListOfProcessedVideos():
+    filenames = next(walk('video_processed'), (None, None, []))[2]
+    return {"Processed Videos": filenames}
 
 
+@app.get("/received-videos")
+def getListOfReceivedVideos():
+    filenames = next(walk('video_received'), (None, None, []))[2]
+    return {"Received Videos": filenames}
 
 
+@app.get("/process-video/{path}")
+def getVideo(path):
+    result = Process(path, saved_model_loaded);
+    result.detect()
+    return {"Download From": "http://localhost:8000/getVideo/{}".format(path)}
---
+++ object_tracker_endpoint.py
@@ -0,0 +1,216 @@
+import os
+
+# comment out below line to enable tensorflow logging outputs
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import time
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+import core.utils as utils
+from core.yolov4 import filter_boxes
+from tensorflow.python.saved_model import tag_constants
+from core.config import cfg
+from PIL import Image
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+# deep sort imports
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+
+
+
+class Process:
+
+    def __init__(self, path, model):
+        self.path = path
+        self.model = model
+
+
+    def detect(self):
+        max_cosine_distance = 0.4
+        nn_budget = None
+        nms_max_overlap = 1.0
+
+        # initialize deep sort
+        model_filename = 'model_data/mars-small128.pb'
+        encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+        # calculate cosine distance metric
+        metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+        # initialize tracker
+        tracker = Tracker(metric)
+
+
+        # load configuration for object detector
+        config = ConfigProto()
+        config.gpu_options.allow_growth = True
+        session = InteractiveSession(config=config)
+        STRIDES = np.array(cfg.YOLO.STRIDES)
+        ANCHORS = utils.get_anchors(cfg.YOLO.ANCHORS, False)
+        XYSCALE = cfg.YOLO.XYSCALE
+        NUM_CLASS = len(utils.read_class_names(cfg.YOLO.CLASSES))
+        input_size = 416
+        video_path = './video_received/{}'.format(self.path)
+        infer = self.model.signatures['serving_default']
+
+        try:
+            vid = cv2.VideoCapture(int(video_path))
+        except:
+            vid = cv2.VideoCapture(video_path)
+
+        out = None
+        width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(vid.get(cv2.CAP_PROP_FPS))
+        codec = cv2.VideoWriter_fourcc(*'MP4V')
+        output_path = './video_processed/{}'.format(self.path)
+        out = cv2.VideoWriter(output_path, codec, fps, (width, height))
+
+        frame_num = 0
+        # while video is running
+        while True:
+            return_value, frame = vid.read()
+            if return_value:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image = Image.fromarray(frame)
+            else:
+                print('Video has ended or failed, try a different video format!')
+                break
+            frame_num +=1
+            print('Frame #: ', frame_num)
+
+            frame_size = frame.shape[:2]
+            image_data = cv2.resize(frame, (input_size, input_size))
+            image_data = image_data / 255.
+            image_data = image_data[np.newaxis, ...].astype(np.float32)
+            start_time = time.time()
+
+
+
+            batch_data = tf.constant(image_data)
+            pred_bbox = infer(batch_data)
+
+
+            for key, value in pred_bbox.items():
+                boxes = value[:, :, 0:4]
+                pred_conf = value[:, :, 4:]
+
+            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+                scores=tf.reshape(
+                    pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+                max_output_size_per_class=50,
+                max_total_size=50,
+                iou_threshold=0.45,
+                score_threshold=0.50
+            )
+
+            # convert data to numpy arrays and slice out unused elements
+            num_objects = valid_detections.numpy()[0]
+            bboxes = boxes.numpy()[0]
+            bboxes = bboxes[0:int(num_objects)]
+            scores = scores.numpy()[0]
+            scores = scores[0:int(num_objects)]
+            classes = classes.numpy()[0]
+            classes = classes[0:int(num_objects)]
+
+            # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+            original_h, original_w, _ = frame.shape
+            bboxes = utils.format_boxes(bboxes, original_h, original_w)
+
+
+
+            # store all predictions in one parameter for simplicity when calling functions
+            pred_bbox = [bboxes, scores, classes, num_objects]
+
+            # read in all class names from config
+            class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+            # by default allow all classes in .names file
+            allowed_classes = list(class_names.values())
+
+            # custom allowed classes (uncomment line below to customize tracker for only people)
+            #allowed_classes = ['person']
+
+            # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+            names = []
+            deleted_indx = []
+            for i in range(num_objects):
+                class_indx = int(classes[i])
+                class_name = class_names[class_indx]
+                if class_name not in allowed_classes:
+                    deleted_indx.append(i)
+                else:
+                    names.append(class_name)
+            names = np.array(names)
+
+
+            # delete detections that are not in allowed_classes
+            bboxes = np.delete(bboxes, deleted_indx, axis=0)
+            scores = np.delete(scores, deleted_indx, axis=0)
+
+
+            # encode yolo detections and feed to tracker
+            features = encoder(frame, bboxes)
+            detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+
+            #initialize color map
+            cmap = plt.get_cmap('tab20b')
+            colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+            # run non-maxima supression
+            boxs = np.array([d.tlwh for d in detections])
+            scores = np.array([d.confidence for d in detections])
+            classes = np.array([d.class_name for d in detections])
+            indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+            detections = [detections[i] for i in indices]
+
+            # Call the tracker
+            tracker.predict()
+            tracker.update(detections)
+
+            # update tracks
+            for track in tracker.tracks:
+                if not track.is_confirmed() or track.time_since_update > 1:
+                    continue
+                bbox = track.to_tlbr()
+                class_name = track.get_class()
+
+                print(str(vid.get(cv2.CAP_PROP_POS_MSEC)))
+                print(class_name)
+                print(track.track_id)
+                print(bbox)
+
+                if(vid.get(cv2.CAP_PROP_POS_MSEC) == 300.276):
+                    print("its time my friend")
+                    cv2.rectangle(frame, (600, 432), (1021, 900), (0, 255, 0), 2)
+
+
+            # draw bbox on screen
+                color = colors[int(track.track_id) % len(colors)]
+
+                color = [i * 255 for i in color]
+                print(color)
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(class_name)+len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+                cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
+
+            # if enable info flag then print details about each track
+
+                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+
+            # calculate frames per second of running detections
+            fps = 1.0 / (time.time() - start_time)
+            print("FPS: %.2f" % fps)
+            result = np.asarray(frame)
+            result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+            out.write(result)
